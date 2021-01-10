614d8b601f598ebf08a1b86e09b978f825415a07
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-10 18:52:49 +0800

    init

--- src/main/kotlin/alert/Application.kt
+++ src/main/kotlin/alert/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AlertApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<AlertApplication>(*args)
+	runApplication<Application>(*args)
 }
