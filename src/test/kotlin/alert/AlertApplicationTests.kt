3815a0909b185b9c0429bee4f92c0575ed57d642
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-10 18:35:14 +0800

    init

---
+++ src/test/kotlin/alert/AlertApplicationTests.kt
@@ -0,0 +1,11 @@
+package alert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlertApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+}
